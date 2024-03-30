b9236b0041af5bd655b9869de4cc4fe8be7741b0
Pascal Baljet <pascal@protone.media>
2024-03-30 21:26:26 +0100

    Update AsResourceTest.php

--- types/AsResourceTest.php
+++ types/AsResourceTest.php
@@ -6,5 +6,5 @@ use function PHPStan\Testing\assertType;
 
 $resource = fopen('php://memory', 'r');
 $variable = random_int(0, 1) !== 0 ? 'string' : $resource;
-assertType('resource', $variable = type($variable)->asResource());
-fclose($resource);
+assertType('resource', type($variable)->asResource());
+$resource && fclose($resource);
