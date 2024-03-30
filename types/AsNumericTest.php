15b7ad49702982e3a37f078432c6f949d5e61cb9
faissaloux <fwahabali@gmail.com>
2024-03-30 01:31:00 +0000

    fix type test

--- types/AsNumericTest.php
+++ types/AsNumericTest.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use function PHPStan\Testing\assertType;
 
 $variable = random_int(0, 1) ? '7415541' : 'not numeric';
-assertType('numeric', type($variable)->asNumeric());
+assertType('float|int|string', type($variable)->asNumeric());
